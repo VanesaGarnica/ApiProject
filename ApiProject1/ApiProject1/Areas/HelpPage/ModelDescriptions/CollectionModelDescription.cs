10eb2c67f049f1ffe40726fe11fc4e32f1177548
VanesaGarnica <60490756+VanesaGarnica@users.noreply.github.com>
2020-07-23 16:30:26 -0300

    commit inicial

---
+++ ApiProject1/ApiProject1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ApiProject1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ApiProject1/ApiProject1/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApiProject1.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
