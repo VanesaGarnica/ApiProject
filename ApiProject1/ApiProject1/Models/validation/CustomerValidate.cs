add295fc520b9e5ae47119ba27088fa834dd1455
VanesaGarnica <60490756+VanesaGarnica@users.noreply.github.com>
2020-07-23 21:31:39 -0300

    validar campos requeridos

---
+++ ApiProject1/ApiProject1/Models/validation/CustomerValidate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiProject1.Models
+{
+    [MetadataType(typeof(Customer.MetaData))]
+    public partial class Customer
+    {
+        
+            sealed class MetaData
+        {
+            [Required]
+            public string UserName;
+            [Required]
+            public string Password;
+
+        }
+    }
+            
+        
+         
+}
